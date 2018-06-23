b9179676be4bbf87e4d91f13435f4e4a0fa31bdb
reishisu <poiponn697@gmail.com>
2018-06-23 18:41:16 +0900

    【新規】MobBase.csにモブの画像を4種類変えられるように設定しました。

--- Phychopas/Assets/Scripts/Mobs/MobBase.cs
+++ Phychopas/Assets/Scripts/Mobs/MobBase.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
 
 public class MobBase : MonoBehaviour {
+    
+    [SerializeField] private Sprite[] dirs = new Sprite[4]; // 前後左右のスプライト画像
+    [SerializeField] private int nowDir;                    // 現在向いている方向
+    private float changeDirTimer;                           // 画像を変えるタイマー
 
 	// Use this for initialization
 	void Start () {
-		
+        // 自身の画像の名前を出力
+        foreach (Sprite s in dirs) {
+            Debug.Log(s.name);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (changeDirTimer > 0.1) {
+            ChangeDirection();
+            changeDirTimer = 0;
+        }
+        changeDirTimer += Time.deltaTime;
 	}
+
+    // モブの向きを変えるメソッド
+    void ChangeDirection() {
+        nowDir++;
+        if (nowDir > 3) nowDir = 0;
+        // 画像をチェンジ
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = dirs[nowDir];
+        // 現在の画像の名前を出力
+        Debug.Log(dirs[nowDir].name);
+    }
+
+    // 移動させるメソッド
+    void Move () {
+        
+    }
 }
