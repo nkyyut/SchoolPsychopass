68109ded81457ed270b8265b3baad749a281f1dc
reishisu <poiponn697@gmail.com>
2018-06-24 09:15:17 +0900

    【新規】キルチェック機能を追加

--- Phychopas/Assets/Scripts/Mobs/MobBase.cs
+++ Phychopas/Assets/Scripts/Mobs/MobBase.cs
@@ -4,44 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
 using System;
+using System.Security.Policy;
 
 public class MobBase : MonoBehaviour {
 
-    // private bool isAlive = true;                         // このキャラが生存しているかどうか
-    private enum Dir { Front, Rear, Left, Right };          // 前後左右の情報
-    [SerializeField] private Sprite[] dirs = new Sprite[4]; // 前後左右のスプライト画像
-    [SerializeField] private Dir nowDir = Dir.Front;        // 現在向いている方向
-    [SerializeField] private float changeDirTimer;          // 画像を変えるタイマー
-
-
-    // Use this for initialization
-    void Start() {
-        // 自身の画像の名前を出力
-        foreach (Sprite s in dirs) {
-            Debug.Log(s.name);
-        }
-    }
-
-    // Update is called once per frame
-    void Update() {
-        if (changeDirTimer > 0.1) {
-            ChangeDirection();
-            changeDirTimer = 0;
-        }
-        changeDirTimer += Time.deltaTime;
-    }
-
-    /***
-     * キャラクターの向きを変えるメソッド
-     */
-    void ChangeDirection() {
-        // 方角の変数を更新
-        nowDir = (int)nowDir < 3 ? ++nowDir : 0;
-        // 画像をチェンジ
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = dirs[(int)nowDir];
-        // 現在の画像の名前を出力
-        Debug.Log(dirs[(int)nowDir].name);
-    }
+    public enum Dir { Front = 0, Rear  = 1,
+                      Left  = 2, Right = 3 };       // 前後左右の情報
+    public Sprite[] dirs = new Sprite[4];           // 前後左右のスプライト画像
+    public Dir nowDir = Dir.Front;                  // 現在向いている方向
+    public float changeDirTiming;                   // 画像を変えるタイミング
+    public float changeDirTimer;                    // 画像を変えるタイマー
 
     /***
      * @param pos マウスのクリックされたポジション
@@ -49,21 +21,34 @@ public class MobBase : MonoBehaviour {
      * @return このキャラクターを殺しても良いか
      */
     public bool KillChack(Vector2 pos, MobBase mb) {
-        // 正面なら即殺す
+        // 正面なら殺せないゲームオーバー
         if (nowDir == Dir.Front) return true;
-        // 左を向いている　かつ　自分より左にいる時　殺す
+        // 左を向いている　かつ　自分より左にいる時　殺せないゲームオーバー
         if (nowDir == Dir.Left  && this.transform.position.x > pos.x) return true;
-        // 右を向いている　かつ　自分より右にいる時　殺す
+        // 右を向いている　かつ　自分より右にいる時　殺せないゲームオーバー
         if (nowDir == Dir.Right && this.transform.position.x < pos.x) return true;
-        // それ以外は殺さない！ == ゲームオーバー
+        // それ以外は殺せる！！
         return false;
     }
 
     /***
      * このキャラを殺す！！メソッド
-     * このモブを消すだけの処理
+     * とりあえずこのモブを消すだけの処理
      */
     public void Killed() {
-        Destroy(this);
+        
+    }
+
+    /***
+     * アイテムを使われた際の振り向くメソッド
+     * @param アイテムの位置
+     */
+    public void ForceChangeDirection(Vector2 pos) {
+        // タイマーをリセット
+        changeDirTimer = 0;
+        // 自身のオブジェクトより左側にある時左にそれ以外は右を向かせる
+        nowDir = (transform.position.x > pos.x) ? Dir.Left : Dir.Right;
+        // 自身のオブジェクトの方角を変える
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = dirs[(int)nowDir];
     }
 }
--- Phychopas/Assets/Scripts/Mobs/MobNormal.cs
+++ Phychopas/Assets/Scripts/Mobs/MobNormal.cs
@@ -6,14 +6,53 @@ using UnityEditor;
 using System;
 
 public class MobNormal : MobBase {
+    
+    private Vector2 min;            // カメラの左下の座標
+    private Vector2 max;            // カメラの右上の座標
+    private System.Random r;        // ランダム用の変数
+    private bool moveLR;            // 左右に移動するための変数   0:Left 1:Right
+    private float width;            // 画像の横幅
+    private float height;           // 画像の縦幅
+    private float moveSpeed;        // モブの移動速度
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        r = new System.Random();
+        moveLR = r.Next(2) == 0 ? true : false;
+        min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        max = Camera.main.ViewportToWorldPoint(Vector2.one);
+        width  = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void Update() {
+        if (changeDirTimer > changeDirTiming) {
+            ChangeDirection();
+            changeDirTimer = 0;
+        }
+        changeDirTimer += Time.deltaTime;
+    }
+
+    /***
+     * キャラクターの向きを変えるメソッド
+     */
+    void ChangeDirection() {
+        int temp = r.Next(4);
+        // 絶対に被らないようにする
+        while ( (int)nowDir == temp ) { temp = r.Next(4); }
+        // 求めたランダムからEnumへ
+        nowDir = (Dir)Enum.ToObject(typeof(Dir), temp);
+        // 向きを変更
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = dirs[(int)nowDir];
+        // 現在の画像の名前を出力
+        Debug.Log(dirs[(int)nowDir].name);
+    }
+
+    /***
+     * キャラクターを移動させる
+     */
+    void Move () {
+        //if ( nowDir == Dir.Left ) {
+            
+        //}
+    }
 }
